83ec08ca8d0944ff23059710f44aeb8115ad4222
TimVu-IBM <96850192+TimVu-IBM@users.noreply.github.com>
2021-12-31 12:14:17 +1100

    Pass the fifth test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -14,15 +14,18 @@ describe('a fahrenheitToCelsius function should', () => {
 
   it('return 10 for 50', () => {
     expect(fahrenheitToCelsius(50)).toBe(10);
-      });
+    });
 
   it('return 100 for 212', () => {
     expect(fahrenheitToCelsius(212)).toBe(100);
-      });
+    });
 
   it('return -40 for -40', () => {
     expect(fahrenheitToCelsius(-40)).toBe(-40);
-      });
-
-  it.todo('return -273.15 for -459.67');
+    });
+      
+  it('return -40 for -40', () => {
+    expect(fahrenheitToCelsius(-459.67)).toBe(-273.15);
+    });
+    
 });
